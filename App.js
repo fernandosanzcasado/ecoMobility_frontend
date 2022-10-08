d449a28fe7867ef04e548c8e81ce61a2275a8d41
PepNav <josep.navarro@estudiantat.upc.edu>
2022-10-08 18:51:34 +0200

    integrating the google maps api

--- App.js
+++ App.js
@@ -1,39 +1,29 @@
-import React, { Component } from "react";
-import { render } from "react-dom";
-import Map from "./Components/Map";
-import credentials from "./credentials";
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import Mapa from "./Components/Map";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
-const mapURL = `https://maps.googleapis.com/maps/api/js?v=3.exp&key=${credentials.mapsKey}`;
-
-class App extends Component() {
-  constructor() {
-    super();
-    this.state = {
-      name: "React",
-    };
-  }
-
-  render() {
-    return (
-      <div>
-        <Map
-          googleMapURL={mapURL}
-          containerElement={<div style={{ height: "400px" }} />}
-          mapElement={<div style={{ height: "100%" }} />}
-          loadingElement={<p>Cargando</p>}
-        />
+export default function App() {
+  return (
+    <View style={StyleSheet.container}>
+      <div className="App">
+        <header className="App-header">
+          <section>
+            <Mapa />
+          </section>
+        </header>
       </div>
-    );
-  }
+    </View>
+  );
 }
 
-render(<App />, document.getElementById("root"));
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: "#fff",
-//     alignItems: "center",
-//     justifyContent: "center",
-//   },
-// });
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
--- Components/Map.js
+++ Components/Map.js
@@ -1,13 +1,20 @@
-import React from "react";
-import { GoogleMap, withScriptjs, withGoogleMap } from "react-google-maps";
+import React, { Component } from "react";
+import GoogleMaps from "simple-react-google-maps";
 
-const Map = (props) => {
-  return (
-    <GoogleMap
-      defaultZoom={10}
-      defaultCenter={{ lat: 41.387397, lng: 2.16949 }}
-    />
-  );
-};
-
-export default withScriptjs(withGoogleMap(Map));
+export default class Mapa extends Component {
+  render() {
+    return (
+      <div className="container">
+        <GoogleMaps
+          apiKey={"AIzaSyD3AAXwJ3MxND_3urIUvXx4PSX1gmmd1ns"}
+          style={{ height: "500px", width: "370px" }}
+          zoom={15}
+          center={{
+            lat: 41.387035,
+            lng: 2.169902,
+          }}
+        />
+      </div>
+    );
+  }
+}
