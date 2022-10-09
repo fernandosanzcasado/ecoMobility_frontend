bdbb7aebde34841712b0ddbee0b67ccdc364c472
PepNav <106391948+PepNav@users.noreply.github.com>
2022-10-09 18:15:24 +0200

    Map integration

---
+++ Components/Maps.js
@@ -0,0 +1,82 @@
+import * as React from "react";
+import * as Location from "expo-location";
+import { StyleSheet, Text, View } from "react-native";
+import MapView, { Marker, Polyline } from "react-native-maps";
+import MapViewDirections from "react-native-maps-directions";
+import { GOOGLE_MAPS_KEY } from "@env";
+
+export default function Mapa() {
+  const [origin, setOrigin] = React.useState({
+    latitude: 41.386976,
+    longitude: 2.169998,
+  });
+
+  /*const [destination, setDestination] = React.useState({
+    latitude: 41.396976,
+    longitude: 2.169998,
+  });*/
+
+  /*Amb aquesta funció pregunto a l'usuari si vol donar-me la ubicació per tal de poder
+  realitzar rutes en temps real, per anar actualitzant-se es pot fer un refresh cada 10-15segons
+  de la posició de l'usuari*/
+  React.useEffect(() => {
+    getLocationPermission();
+  }, []);
+
+  async function getLocationPermission() {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== "granted") {
+      alert("Permission denied");
+      return;
+    } else {
+      let location = await Location.getCurrentPositionAsync({});
+      const current = {
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+      };
+      setOrigin(current);
+    }
+  }
+
+  return (
+    <MapView
+      style={styles.map}
+      initialRegion={{
+        latitude: origin.latitude,
+        longitude: origin.longitude,
+        //La quantitat de distància d'est a oest mesurada en graus a mostrar per a la regió del mapa
+        latitudeDelta: 0.09,
+        longitudeDelta: 0.04,
+      }}
+    >
+      <Marker
+        draggable
+        coordinate={origin}
+        onDragEnd={(direction) => setOrigin(direction.nativeEvent.coordinate)}
+      />
+      {/*
+      <Marker
+        draggable
+        coordinate={destination}
+        onDragEnd={(direction) =>
+          setDestination(direction.nativeEvent.coordinate)
+        }
+      />
+      <MapViewDirections
+        origin={origin}
+        destination={destination}
+        apikey={GOOGLE_MAPS_KEY}
+        strokeColor="green"
+        strokeWidth={3}
+      />
+    */}
+    </MapView>
+  );
+}
+
+const styles = StyleSheet.create({
+  map: {
+    height: "100%",
+    width: "100%",
+  },
+});
--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,20 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
+    plugins: [
+      /*Activo el modul react-native-dotenv i modifiquem les característiques: nom, pat, etc*/
+      [
+        "module:react-native-dotenv",
+        {
+          moduleName: "@env",
+          path: ".env",
+          blacklist: null,
+          whitelist: null,
+          safe: false,
+          allowUndefined: true,
+        },
+      ],
+    ],
   };
 };
