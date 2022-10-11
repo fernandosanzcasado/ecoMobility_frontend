8cfb4fec5cb84295e33a269a0ed4e20ae6deae38
fernandosanzcasado <fernandosanzeleven@gmail.com>
2022-10-11 23:57:21 +0200

    Navigation working and PruebaScreen

--- App.js
+++ App.js
@@ -4,13 +4,20 @@ import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import Mapa from "./Components/Maps";
+import { NavigationContainer } from "@react-navigation/native";
+import NavigationTab from "./src/navigation/NavigationTab"
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Mapa />
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <NavigationTab/>
+
+      {/* <View style={styles.container}>
+        <Mapa />
+        <StatusBar style="auto" />
+      </View> */}
+
+    </NavigationContainer>
   );
 }
 
---
+++ src/screens/PruebaScreen.js
@@ -0,0 +1,16 @@
+import { SafeAreaView, Text } from 'react-native'
+import React from 'react'
+
+export default function PruebaScreen() {
+  return (
+    <SafeAreaView>
+      <Text>ESTA ES UNA SCREEN DE PRUEBA</Text>
+      <Text>ESTA ES UNA SCREEN DE PRUEBA</Text>
+      <Text>ESTA ES UNA SCREEN DE PRUEBA</Text>
+      <Text>ESTA ES UNA SCREEN DE PRUEBA</Text>
+      <Text>ESTA ES UNA SCREEN DE PRUEBA</Text>
+      <Text>ESTA ES UNA SCREEN DE PRUEBA</Text>
+      <Text>ESTA ES UNA SCREEN DE PRUEBA</Text>
+    </SafeAreaView>
+  )
+}
