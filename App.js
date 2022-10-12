61fabe11a1637137ca79e78513667a1ea4644bec
fernandosanzcasado <fernandosanzeleven@gmail.com>
2022-10-12 10:49:27 +0200

    Cleaned App.js

--- App.js
+++ App.js
@@ -1,22 +1,14 @@
 import "react-native-gesture-handler"; // Import necesario para el react-navigation
 
 import React from "react";
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import Mapa from "./Components/Maps";
 import { NavigationContainer } from "@react-navigation/native";
-import NavigationTab from "./src/navigation/NavigationTab"
+import NavigationTab from "./src/navigation/NavigationTab";
 
 export default function App() {
   return (
     <NavigationContainer>
-      <NavigationTab/>
-
-      {/* <View style={styles.container}>
-        <Mapa />
-        <StatusBar style="auto" />
-      </View> */}
-
+      <NavigationTab />
     </NavigationContainer>
   );
 }
