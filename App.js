45950f28b130d9e90306fca1d5370005f3f6a41b
carla.canovas <carla.canovas@est.fib.upc.edu>
2022-10-17 09:52:11 +0200

    #93 Adding navigation

--- App.js
+++ App.js
@@ -1,24 +1,20 @@
 import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer, StackActions } from "@react-navigation/native";
 import Profile from "./src/screens/Profile";
 import EditProfile from "./src/screens/EditProfile";
 
-const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator
-        options={{
-          tabBarStyle: { display: "none" },
-        }}
-      >
-        <Tab.Screen name="Profile" component={Profile} />
-        <Tab.Screen name="EditProfile" component={EditProfile} />
-      </Tab.Navigator>
+      <Stack.Navigator>
+        <Stack.Screen name="Profile" component={Profile}></Stack.Screen>
+        <Stack.Screen name="EditProfile" component={EditProfile}></Stack.Screen>
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
